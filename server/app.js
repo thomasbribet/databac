ab9186cac01144b7997815c1f63a3181cada13aa
Thomas <thomas.bribet@gmail.com>
2021-11-08 11:31:05 +0100

    db connexion ok

--- server/app.js
+++ server/app.js
@@ -1,11 +1,23 @@
 const helmet = require("helmet");
 const cors = require("cors");
+const history = require("connect-history-api-fallback");
+require("dotenv").config();
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(history());
 app.use(express.static(__dirname + "/dist"));
+
+mongoose
+  .connect(process.env.DB_CONNSTRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connection: OK"))
+  .catch((error) => handleError(error));
 
 module.exports = app;
